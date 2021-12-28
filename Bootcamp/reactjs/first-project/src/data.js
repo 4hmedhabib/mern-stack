26cb097a5d9ff7917b9abe5618c6b2ac9ce09956
ahmedhabiib <info.ahmed.developer@gmail.com>
2021-12-28 17:50:19 +0300

    added first lesson

---
+++ Bootcamp/reactjs/first-project/src/data.js
@@ -0,0 +1,51 @@
+const store = [
+  {
+    name: "Apple",
+    image: "/images/Apples.jpeg",
+    description: "Organic Granny Apples 3 Lb Pouch",
+
+    quantity: 15,
+    price: 4.96,
+  },
+  {
+    name: "Bananas",
+    image: "/images/Bananas.jpeg",
+    description: "Organic Bananas, Bunch",
+    quantity: 17,
+    price: 1.63,
+  },
+  {
+    name: "Grapes",
+    image: "/images/Grapes.jpeg",
+    quantity: 51,
+    price: 5.98,
+  },
+  {
+    name: "Strawberries",
+    image: "/images/Strawberries.jpeg",
+    description: "Fresh USDA Organic Strawberries, 16 Oz",
+    quantity: 105,
+    price: 4.98,
+  },
+  {
+    name: "Carrots",
+    image: "/images/Carrots.jpeg",
+    description: "Organic Whole Carrots, 2 Lb Bag",
+    quantity: 92,
+    price: 2.06,
+  },
+  {
+    name: "Tomatoes",
+    image: "/images/tomatoes.jpeg",
+    description: "Organic Grape Tomatoes, 10 oz",
+    quantity: 26,
+    price: 2.92,
+  },
+  {
+    name: "Avocados",
+    image: "/images/avocados.jpeg",
+    description: "Organic Medium Hass Avocados, 3-5 Count Bag",
+    quantity: 66,
+    price: 4.96,
+  },
+];
