550b5e2e517245486fdca9a2d825e9e6c0b9b3a1
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-01 08:46:58 +0300

    finished

--- Bootcamp/ReactJs/user-servey/src/components/UI/ErrorModal.jsx
+++ Bootcamp/ReactJs/user-servey/src/components/UI/ErrorModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
   return (
@@ -16,18 +17,16 @@ const Modal = (props) => {
   return (
     <div id="modal" className={styles.modal}>
       <Card>
-        <div>
-          <div className={styles.title}>
-            <h1>{props.title}</h1>
-          </div>
-          <div className={styles.content}>
-            <p>{props.content}</p>
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.btn} onClick={props.onClose}>
-              Okay
-            </button>
-          </div>
+        <div className={styles.title}>
+          <h1>{props.title}</h1>
+        </div>
+        <div className={styles.content}>
+          <p>{props.content}</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button className={styles.btn} onClick={props.onClose}>
+            Okay
+          </button>
         </div>
       </Card>
     </div>
@@ -37,12 +36,18 @@ const Modal = (props) => {
 const ErrorModal = (props) => {
   return (
     <div id="error-modal">
-      <Backdrop onClose={props.onClose} />
-      <Modal
-        title={props.title}
-        content={props.content}
-        onClose={props.onClose}
-      />
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        document.querySelector("#root-backdrop")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          content={props.content}
+          onClose={props.onClose}
+        />,
+        document.querySelector("#root-modal")
+      )}
     </div>
   );
 };
