d1ce0d2265a7db467dacf5146a46c2bb324953b8
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-14 09:13:18 +0300

    added: rules of react hooks

--- Bootcamp/ReactJs/hook-project/src/App.js
+++ Bootcamp/ReactJs/hook-project/src/App.js
@@ -1,34 +1,19 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useContext, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import UseContext from "./components/hooks/useContext";
 import Login from "./components/Login/Login";
 
+import AuthContext from "./store/auth-state";
+
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (localStorage.getItem("isLoggedIn") === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
-    setIsLoggedIn(false);
-  };
+  const authCtx = useContext(AuthContext);
 
   return (
     <Fragment>
-      <Header isLoggedIn={isLoggedIn} onLogout={logoutHandler} />
+      <Header />
       <main>
-        {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {authCtx.isLoggedIn && <Home />}
+        {!authCtx.isLoggedIn && <Login />}
       </main>
       {/* <UseContext /> */}
       {/* <UseReducer /> */}
---
+++ Bootcamp/ReactJs/hook-project/src/Rules.md
@@ -0,0 +1,10 @@
+# Rules of React Hooks
+
+---
+
+1. Only call React Hooks In React Functions
+   - React Component Fuction
+   - Custom Hooks
+2. Only Call React Hooks at the top level
+   - Don’t call Hooks inside loops, conditions, or nested functions.
+   - Don't call them in any block statements
